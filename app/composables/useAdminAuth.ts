91cad3d58d069ef065c917cf1392adcb9d0d782b
Sekiuu <sega.pamma@gmail.com>
2026-04-09 20:46:34 +0700

    Update useAdminAuth.ts
    - add {fetch : refreshSession} = useUserSession()
    to refersh session for middleware

--- app/composables/useAdminAuth.ts
+++ app/composables/useAdminAuth.ts
@@ -1,21 +1,22 @@
 import type { UserItem } from "~/types"
 
 export const useAdminAuth = () => {
+    const { fetch: refreshSession } = useUserSession()
     const logout = async () => {
         await $fetch('/api/auth/logout', {
             method: 'POST'
         })
+        await refreshSession()
     }
     const signin = async (name: string, password: string) => {
         await $fetch('/api/auth/login', {
             method: 'POST',
             body: { name, password }
         })
+        await refreshSession()
     }
-    const session = useUserSession()
 
     return {
-        session,
         logout,
         signin
     }
