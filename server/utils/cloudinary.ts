ba561fb7ef5a136fd15e0072abfdb043101311ba
Sekiuu <sega.pamma@gmail.com>
2026-04-25 23:58:31 +0700

    add deging for cloudinary

--- server/utils/cloudinary.ts
+++ server/utils/cloudinary.ts
@@ -1,6 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 const config = useRuntimeConfig()
+
+if (!config.public.cloudinary.cloudName) throw new Error('Missing cloudinary.cloudName')
+if (!config.cloudinary.apiKey) throw new Error('Missing cloudinary.apiKey')
+if (!config.cloudinary.apiSecret) throw new Error('Missing cloudinary.apiSecret')
+
 cloudinary.config({
     cloud_name: config.public.cloudinary.cloudName,
     api_key: config.cloudinary.apiKey,
